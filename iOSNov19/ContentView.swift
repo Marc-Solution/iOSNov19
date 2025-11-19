61a7a828d1c6b911dd422b932aa6627e90cd2f25
Marco Deb <marco.deb@gmail.com>
2025-11-19 09:22:18 +0100

    Ändrat text

--- iOSNov19/ContentView.swift
+++ iOSNov19/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tjena Tjena")
+            Text("Tjena Tjena")
         }
         .padding()
     }
