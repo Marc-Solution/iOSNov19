c9c049b4de308983d6d14b7aeef1c60035a200ff
Marco Deb <marco.deb@gmail.com>
2025-11-19 09:14:15 +0100

    Initial Commit

---
+++ iOSNov19/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSNov19
+//
+//  Created by Marco Deb on 2025-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSNov19/iOSNov19App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSNov19App.swift
+//  iOSNov19
+//
+//  Created by Marco Deb on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct iOSNov19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
